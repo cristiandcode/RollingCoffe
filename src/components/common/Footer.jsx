f1cc7dd717e7a781668fec8a7f5203e5dc1b7d5b
Emilse Arias <emilsearias@gmail.com>
2024-02-01 17:40:51 -0300

    agregamos los componentes comunes Footer y Menu, luego el componente Inicio en la carpeta pages

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <p>aqui va el maquetado del footer</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,9 @@
+const Inicio = () => {
+    return (
+        <div>
+            <p>aqui va el main de la pagina principal</p>
+        </div>
+    );
+};
+
+export default Inicio;
