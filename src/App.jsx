e2bbf73a92e4fe9f4964ea8c913d0b9af265ea41
Emilse Arias <emilsearias@gmail.com>
2024-02-01 18:26:59 -0300

    agregamos las opciones del menu y el logo de la empresa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/common/Menu'
+import Footer from './components/common/Footer'
+import Inicio from './components/pages/Inicio'
+import Administrador from './components/pages/Administrador'
 
 function App() {
  
   return (
     <>
-      <h1>hola mundo</h1>
+      <Menu></Menu>
+      <Inicio></Inicio>
+      {/* <Administrador></Administrador> */}
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,10 @@
+
+const Error404 = () => {
+    return (
+        <div>
+            <p>aqui va el maquetado del error 404</p>
+        </div>
+    );
+};
+
+export default Error404;
