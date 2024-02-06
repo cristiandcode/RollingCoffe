6291cced05626e80cbd9e895f645b2c44a252ca6
Emilse Arias <emilsearias@gmail.com>
2024-02-06 16:32:32 -0300

    agregue el maquetado de Inicio, Administrador, Error404

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Menu from './components/common/Menu'
 import Footer from './components/common/Footer'
 import Inicio from './components/pages/Inicio'
 import Administrador from './components/pages/Administrador'
+import Error404 from './components/pages/Error404';
 
 function App() {
  
   return (
     <>
       <Menu></Menu>
-      <Inicio></Inicio>
+      {/* <Inicio></Inicio> */}
       {/* <Administrador></Administrador> */}
+      {/* <Error404></Error404> */}
       <Footer></Footer>
     </>
   )
--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,9 +1,14 @@
-
+import { Button } from 'react-bootstrap';
+import error from '../../assets/error404.png'
 const Error404 = () => {
     return (
+        <section className="mainSection text-center">
+        <img src={error} alt="error 404" />
         <div>
-            <p>aqui va el maquetado del error 404</p>
+        <Button variant='primary' >Volver al inicio</Button>
+
         </div>
+    </section>
     );
 };
 
