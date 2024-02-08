ef1a508d4ff1a3184c04274fb486179fb1bd8ad3
Emilse Arias <emilsearias@gmail.com>
2024-02-08 16:41:53 -0300

    agregue el maquetado de detalleProducto y FormularioProducto

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
 import Administrador from "./components/pages/Administrador";
+import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Error404 from "./components/pages/Error404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetalleProducto from "./components/pages/DetalleProducto";
 
 function App() {
   return (
@@ -14,8 +16,9 @@ function App() {
       <Menu></Menu>
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/detalleProducto" element={<DetalleProducto></DetalleProducto>}></Route>
         <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
-        <Route exact path="/administrador/crear" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/crear" element={<FormularioProducto></FormularioProducto>}></Route>
         <Route exact path="/administrador/editar" element={<Administrador></Administrador>}></Route>
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -1,4 +1,5 @@
 import { Col, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const CardProducto = () => {
  
@@ -11,11 +12,11 @@ const CardProducto = () => {
         <Card.Body>
         <Card.Title className="primary-font">Capuchino</Card.Title>
         <Card.Text>
-          Descripción: Descripción: Espuma de leche cremosa sobre un espresso fuerte. <br className="mb-2"/> 
+          Descripción: Espuma de leche cremosa sobre un espresso fuerte. <br className="mb-2"/> 
           <span className="fw-bold">Precio: $350</span></Card.Text>
         </Card.Body>
         <Card.Footer className="text-end">
-        <Button variant='success' className="me-2" >Ver más</Button>
+        <Link className='btn btn-success me-2' to='/detalleProducto' >Ver más</Link>
       </Card.Footer>
       </Card>
     </Col>
