fc96e276df6ff05417a69d7b849e9d268abd497e
Emilse Arias <emilsearias@gmail.com>
2024-02-15 17:42:54 -0300

    agregamos la logica necesaria para solicitar a la api agregar un producto

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,12 +2,30 @@ const URI_PRODUCTOS= import.meta.env.VITE_API_PRODUCTOS;
 
 console.log(URI_PRODUCTOS);
 
+//GET
 export const leerProductosAPI = async()=>{
     try{
         const respuesta = await fetch(URI_PRODUCTOS);
         const listaProductos = await respuesta.json();
         return listaProductos;
     }catch (error){
+        console.log(error)
+    }
+}
+
+// POST
+export const crearProductoAPI = async(productoNuevo)=>{
+    try {
+        const respuesta = await fetch(URI_PRODUCTOS, {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(productoNuevo)
+        })
+        console.log(respuesta);
+        return respuesta
+    } catch (error) {
         console.log(error)
     }
 }
