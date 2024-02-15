abe77ad261792cb03aab0b1139a4374827d90781
Emilse Arias <emilsearias@gmail.com>
2024-02-15 19:10:29 -0300

    agregue el maquetado de la pagina del login

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Error404 from "./components/pages/Error404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DetalleProducto from "./components/pages/DetalleProducto";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/detalleProducto" element={<DetalleProducto></DetalleProducto>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
         <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
         <Route exact path="/administrador/crear" element={<FormularioProducto></FormularioProducto>}></Route>
         <Route exact path="/administrador/editar" element={<Administrador></Administrador>}></Route>
