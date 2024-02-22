24bfae48cd110f0d19576dad04e38a8c51112a36
Emilse Arias <emilsearias@gmail.com>
2024-02-22 17:02:52 -0300

    agregue la logica de la pagina de inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,7 +1,23 @@
 import { Container, Row } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
+import { useEffect, useState } from "react";
+import { leerProductosAPI } from "../../helpers/queries";
 
 const Inicio = () => {
+  const [productos, setProductosInicio] = useState([]);
+  useEffect(() => {
+    leerProductosInicio();
+  }, []);
+
+  const leerProductosInicio = async () => {
+    try {
+      const productosAPIinicio = await leerProductosAPI();
+      setProductosInicio(productosAPIinicio);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="mainSection">
       <img
@@ -14,11 +30,9 @@ const Inicio = () => {
         <hr />
     
           <Row>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
-            <CardProducto></CardProducto>
+         {
+          productos.map((producto)=>  <CardProducto key={producto.id} producto={producto}></CardProducto>)
+         }
           </Row>
        
       </Container>
