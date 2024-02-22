d425aa265e4f0800bf4963118686577f5272bfe5
Emilse Arias <emilsearias@gmail.com>
2024-02-22 18:22:52 -0300

    agregamos la funcion para realizar el login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,8 @@
 import { Form, Button, Container, Card } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { login } from "../../helpers/queries";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const {
@@ -7,9 +10,26 @@ const Login = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const navegacion = useNavigate()
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (usuario) => {
+    if(login(usuario)){
+      //usuario logueado
+      Swal.fire({
+        title: "Usuario logueado",
+        text: `El usuario ${usuario.email} fue logueado correctamente`,
+        icon: "success",
+      });
+      //redireccionar a la pagina del administrador
+      navegacion('/administrador')
+    }else{
+      //el usuario no fue logueado
+      Swal.fire({
+        title: "Error en el login",
+        text: "El email o password son incorrectos",
+        icon: "error",
+      });
+    }
   };
 
   return (
@@ -21,14 +41,26 @@ const Login = () => {
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="email"
+                type="text"
                 placeholder="Ingrese un email"
-                {...register("mail", {
-                  required: "El nombre de usuario es obligatorio",
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  minLength: {
+                    value: 4,
+                    message: "El email debe contener al menos 4 caracteres",
+                  },
+                  maxLength: {
+                    value: 250,
+                    message: "El email debe contener como máximo 250 caracteres",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                    message: "Ingrese una dirección de correo electrónico válida",
+                  },
                 })}
               />
               <Form.Text className="text-danger">
-                {errors.mail?.message}
+                {errors.email?.message}
               </Form.Text>
             </Form.Group>
 
@@ -38,7 +70,16 @@ const Login = () => {
                 type="password"
                 placeholder="Password"
                 {...register("password", {
-                  required: "El nombre de password es obligatorio",
+                  required: "El password es obligatorio",
+                  minLength: { value: 8, message: "el minimo es de 8 caracteres" },
+                  maxLength: {
+                    value: 12,
+                    message: "el maximo es de 15 caracteres",
+                  },
+                  pattern: {
+                    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                    message: "El password debe contener al menos una letra mayúscula, una letra minúscula y un número",
+                  },
                 })}
               />
               <Form.Text className="text-danger">
