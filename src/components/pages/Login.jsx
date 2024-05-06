47b74672ed0ce58d484e3dd84871c869082dcad4
Emilse Arias <emilsearias@gmail.com>
2024-05-06 18:36:30 -0300

    agregue la validacion del campo del password en el form de login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,11 +2,15 @@ import { Container, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
- const {register, handleSubmit,  formState: { errors }} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-const onSubmit = (usuario)=>{
-    console.log(usuario)
-}
+  const onSubmit = (usuario) => {
+    console.log(usuario);
+  };
 
   return (
     <Container className="my-5">
@@ -14,24 +18,55 @@ const onSubmit = (usuario)=>{
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Correo electronico:</Form.Label>
-          <Form.Control type="email" placeholder="Ej: juan@mail.com" {
-            ...register("email",{
-                required: "El correo es obligatorio",
-                pattern:{
-                    value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                    message:"El correo debe ser un email valido Ej: nombre@mail.com"
-                }
-            })
-          }/>
+          <Form.Control
+            type="email"
+            placeholder="Ej: juan@mail.com"
+            {...register("email", {
+              required: "El correo es obligatorio",
+              minLength: {
+                value: 4,
+                message: "El email debe contener al menos 4 caracteres",
+              },
+              maxLength: {
+                value: 250,
+                message: "El email debe contener como máximo 250 caracteres",
+              },
+              pattern: {
+                value:
+                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                message:
+                  "El correo debe ser un email valido Ej: nombre@mail.com",
+              },
+            })}
+          />
           <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Contraseña:</Form.Label>
-          <Form.Control type="password" placeholder="ingrese una contraseña" />
+          <Form.Control
+            type="password"
+            placeholder="ingrese una contraseña"
+            {...register("password", {
+              required: "El password es obligatorio",
+              minLength: { value: 8, message: "el minimo es de 8 caracteres" },
+              maxLength: {
+                value: 12,
+                message: "el maximo es de 15 caracteres",
+              },
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                message:
+                  "El password debe contener al menos una letra mayúscula, una letra minúscula y un número",
+              },
+            })}
+          />
         </Form.Group>
-       
+
         <Form.Group className="mb-3">
-          <Form.Text className="text-danger">Algun error</Form.Text>
+          <Form.Text className="text-danger">
+            {" "}
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
         <Button variant="success" type="submit">
           Enviar
